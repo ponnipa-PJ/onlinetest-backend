3d9b55b13f43cf16009a6170e279f2a10f3582c3
ponnipa <33538593+ponnipa-PJ@users.noreply.github.com>
2022-01-18 12:56:35 +0700

    update get data to json

--- apis/answers/getquestionandchoiceadmin.php
+++ apis/answers/getquestionandchoiceadmin.php
@@ -1,73 +1,66 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header('Access-Control-Allow-Methods, GET');
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../../config/database.php';
-include_once '../../class/answers.php';
 include_once '../../class/questionsandanswers.php';
+include_once '../../class/answers.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$ques = new QuestionAndAnswer($db);
-$items = new Answer($db);
+$items = new QuestionAndAnswer($db);
+$answer = new Answer($db);
 
-$ques->subject_id = isset($_GET['subject_id']) ? $_GET['subject_id'] : die();
-$ques->part_id = isset($_GET['part_id']) ? $_GET['part_id'] : die();
-
-$stmt = $ques->getAllAnswersandQuestions();
-
+$items->subject_id = isset($_GET['subject_id']) ? $_GET['subject_id'] : die();
+$items->part_id = isset($_GET['part_id']) ? $_GET['part_id'] : die();
+$stmt = $items->getAllAnswersandQuestions();
 $itemCount = $stmt->rowCount();
-$ans = [];
-// echo $itemCount;
+
 
 if ($itemCount > 0) {
 
     $productArr = array();
-    $detailArr = array();
     $productArr["body"] = array();
     $productArr["itemCount"] = $itemCount;
-    
+
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // print_r($row['question_id']);
-        $items->question_id = $row['question_id'];
-        $stmtd = $items->getAnswersByquestionID();
-        $detailCount = $stmtd->rowCount();
-        $ans = $row['answer'];
-        
-        if ($detailCount > 0) {
-            $detailArr["body"] = array();
-            while ($detail = $stmtd->fetch(PDO::FETCH_ASSOC)) {
-                // print_r($detail);
-                $check=$ques->answer_id = $detail['id'];
-                $check=$ques->question_id = $detail['question_id'];
-                $check=$ques->getCheckanswer();
-                
-                $d = array(
-                    "answer_id" => $detail['id'],
-                    "question_id" => $detail['question_id'],
-                    "name" => $detail['name'],
+
+        $answer->question_id = $row['question_id'];
+        $stmtanswer = $answer->getAnswersByquestionID();
+        $answerArrCount = $stmtanswer->rowCount();
+
+        if ($answerArrCount > 0) {
+            $answerArr = array();
+            $answerArr["body"] = array();
+            while ($ans = $stmtanswer->fetch(PDO::FETCH_ASSOC)) {
+                $check = $items->answer_id = $ans['id'];
+                $check = $items->question_id = $ans['question_id'];
+                $check = $items->getCheckanswer();
+
+                $a = array(
+                    "answer_id" => $ans['id'],
+                    "question_id" => $ans['question_id'],
+                    "name" => $ans['name'],
                     "checked" => $check,
                 );
-                array_push($detailArr["body"], $d);
+                array_push($answerArr["body"], $a);
             }
         }
         extract($row);
         $e = array(
-            "questions_answers_id" => $questions_answers_id,
             "question_id" => $question_id,
             "name" => $name,
-            "answer" => $answer,
-            "details" => $detailArr["body"]
+            "details" => $answerArr["body"]
         );
+
         array_push($productArr["body"], $e);
     }
     echo json_encode($productArr["body"]);
 } else {
     // http_response_code(404);
-    // echo json_encode(
-    //     array("message" => "No record found.")
-    // );
     echo false;
 }
